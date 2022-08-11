d97f1474548b8f08143f12422f64c5add44e8fd0
Yosua Nicolaus <cryodawn257@gmail.com>
2022-08-11 15:39:27 +0700

    create AppTable (lobby for online match)

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,7 +1,24 @@
+import AppTable from "./AppTable";
+
 function App() {
   return (
     <div>
-      <div>hello world</div>
+      <div className="display-6 p-3 d-flex shadow text-bg-primary">
+        LogiChess
+      </div>
+      <main className="p-3 row">
+        <div className="col-md-6 shadow">
+          <div className="lead">Lobby</div>
+          <AppTable />
+        </div>
+
+        <div className="col-md-6">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit ipsam
+          quaerat veniam placeat perferendis adipisci, voluptatibus laboriosam
+          repudiandae maxime suscipit magnam porro amet eaque vero, architecto
+          reprehenderit ad modi possimus?
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/pages/App/AppTable.jsx
@@ -0,0 +1,52 @@
+function AppTable() {
+  const dummyData = [
+    {
+      username: "johndoe1995",
+      elo: 1348,
+      time: "10+5",
+    },
+    {
+      username: "BobRoss",
+      elo: 1801,
+      time: "2+1",
+    },
+    {
+      username: "Jack-5",
+      elo: 1500,
+      time: "3+0",
+    },
+    {
+      username: "CaroKhanPlease",
+      elo: 927,
+      time: "10+0",
+    },
+  ];
+
+  return (
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Username</th>
+          <th scope="col">ELO</th>
+          <th scope="col">Time Control</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* TODO: replace dummyData with actual lobby data */}
+        {/* TODO: make each table row a clickable link */}
+        {dummyData &&
+          dummyData.map((data, idx) => (
+            <tr key={`apptable-${idx}`}>
+              <th scope="row">{idx}</th>
+              <td>{data.username}</td>
+              <td>{data.elo}</td>
+              <td>{data.time}</td>
+            </tr>
+          ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default AppTable;
