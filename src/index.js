9ace60bf939aca3114a6b03e862531099d3b61de
Yosua Nicolaus <cryodawn257@gmail.com>
2022-08-11 14:27:30 +0700

    add dependencies & routes, move App to pages dir

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+import "bootswatch/dist/flatly/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./pages/App/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
