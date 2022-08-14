8c7ea7b0a062ec3b1942e08841b08b36e8b7153c
Yosua Nicolaus <cryodawn257@gmail.com>
2022-08-15 06:41:23 +0700

    add firebase for user authentication

---
+++ src/helper/firebase-config.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
