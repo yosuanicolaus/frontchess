e9cbaedf4fdf412a44d5656105eefd0a5482f589
Yosua Nicolaus <cryodawn257@gmail.com>
2022-08-15 21:47:41 +0700

    add {username} when post /game/new

--- src/helper/api.js
+++ src/helper/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { auth } from "./auth";
 
 // axios = axios.create({ baseURL: "http://localhost:3001/" });
 axios.defaults.baseURL = "http://localhost:3001/";
@@ -10,7 +11,8 @@ export async function getAllGames() {
 }
 
 export async function postGameNew() {
-  const response = await axios.post("/game/new");
+  const username = auth.currentUser.displayName || auth.currentUser.uid;
+  const response = await axios.post("/game/new", { username });
   const data = await response.data;
   return data;
 }
