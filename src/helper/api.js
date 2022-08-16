dc8834fbd58d349bd62c08d24df4482e97b3d7a2
Yosua Nicolaus <cryodawn257@gmail.com>
2022-08-16 13:57:08 +0700

    refactor api functions, add getGameByID

--- src/helper/api.js
+++ src/helper/api.js
@@ -5,20 +5,46 @@ import { auth } from "./auth";
 axios.defaults.baseURL = "http://localhost:3001/";
 
 export async function getAllGames() {
-  const response = await axios.get("/");
-  const data = await response.data;
-  return data;
+  return apiGet("/");
 }
 
 export async function postGameNew(timeControl) {
-  const username = auth.currentUser.displayName || auth.currentUser.uid;
-  const response = await axios.post("/game/new", { username, timeControl });
-  const data = await response.data;
-  return data;
+  const user = auth.currentUser;
+  const username = user.displayName || user.uid;
+  return apiPost("/game/new", { username, timeControl });
+}
+
+export async function deleteGame(gameID) {
+  // TODO:
+  // backend: create api route /delete/game/:id
+  // then call that route here
+  console.log("deleted game with id", gameID);
+}
+
+export async function getGameByID(gameID) {
+  return apiGet(`/game/${gameID}`);
 }
 
 export async function apiTest() {
-  const response = await axios.get("/test");
-  const data = await response.data;
-  return data;
+  return apiGet("/test");
+}
+
+async function apiGet(path) {
+  try {
+    const response = await axios.get(path);
+    const data = await response.data;
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function apiPost(path, postData) {
+  try {
+    const response = await axios.post(path, postData);
+    const data = await response.data;
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
