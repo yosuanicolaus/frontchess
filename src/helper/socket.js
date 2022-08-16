129849d1ad7171e7f378a5ce42f5e819dd23aa8a
Yosua Nicolaus <cryodawn257@gmail.com>
2022-08-16 19:57:52 +0700

    emit "join" just after creating game

--- src/helper/socket.js
+++ src/helper/socket.js
@@ -2,4 +2,9 @@ import { io } from "socket.io-client";
 
 const socket = io("http://localhost:3003/");
 
+socket.on("error", (message) => {
+  console.log("error handled in /helper/socket.js");
+  console.log(message);
+});
+
 export { socket };
