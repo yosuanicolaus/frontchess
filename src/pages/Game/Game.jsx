e263c029b1d0789fa9b5f78e1e4fa149045b7bf0
Yosua Nicolaus <cryodawn257@gmail.com>
2022-08-17 11:16:04 +0700

    create Game's socket listener & "join" emit, +
    add <Loading /> for when the gameDB state is null
    
    show gameDB and chess state as JSON string for debugging purposes

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -1,13 +1,57 @@
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { socket } from "../../helper/socket";
+import Loading from "../../components/Loading";
+// import { auth } from "../../helper/auth";
+// TODO: set what user can control with auth
 
 function Game() {
   const { id } = useParams();
+  const [gameDB, setGameDB] = useState(null);
+  const [chess, setChess] = useState(null);
+
+  useEffect(() => {
+    socket.on("update-game", (game) => {
+      console.log("updating gameDB state");
+      setGameDB(game);
+    });
+
+    socket.on("update-chess", (chess) => {
+      console.log("updating chess state");
+      setChess(chess);
+    });
+
+    socket.on("log", (message) => {
+      console.log(message);
+    });
+
+    return () => {
+      console.log("leaving, turning off socket listener");
+      socket.off("update-game");
+      socket.off("update-chess");
+      socket.off("log");
+    };
+  }, []);
+
+  useEffect(() => {
+    socket.emit("join", id);
+  }, [id]);
+
+  if (!gameDB)
+    return (
+      <main className="flex-grow-1 d-flex justify-content-center align-items-center">
+        <Loading text={"fetching game from DB..."} />
+      </main>
+    );
 
   return (
-    <>
+    <main>
       <h1>Hello! this is game room</h1>
       <div>Game ID: {id}</div>
-    </>
+      {gameDB && <div>{JSON.stringify(gameDB)}</div>}
+      <hr />
+      {chess && <div>{JSON.stringify(chess.getMoves())}</div>}
+    </main>
   );
 }
 
