3b64ea91b3d72903f5c0167753df2a10bf90575a
Yosua Nicolaus <cryodawn257@gmail.com>
2022-08-20 12:33:06 +0700

    remodel Test page and make sure all api func works

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,34 +1,75 @@
-import {
-  getAllGames,
-  getGame,
-  postGameNew,
-  postUserNew,
-  getUser,
-} from "../helper/api";
-
-const testData = [
-  [getAllGames],
-  [getGame, "somegameid"],
-  [postGameNew, "12+6"],
-  [postUserNew, "qwertyzxcvnice25"],
-  [getUser, "qwertyzxcvnice25"],
-];
+import { useApi } from "../helper/api";
+import React, { useState } from "react";
 
 function Test() {
+  const [log, setLog] = useState("");
+  const {
+    getAllData,
+    getChat,
+    getGame,
+    getGameRandomOpen,
+    getUser,
+    postChatNew,
+    postChatNewMessage,
+    postGameJoin,
+    postGameLeave,
+    postGameNew,
+    postGameReady,
+    postGameStart,
+    postUserNew,
+  } = useApi();
+
+  const testData = [
+    [getAllData],
+    [getGame, "MeUWJyMeS6"],
+    [getGameRandomOpen],
+    [postGameNew, "10+5"],
+    [postGameJoin, "MeUWJyMeS6"],
+    [postGameLeave, "MeUWJyMeS6"],
+    [postGameReady, "MeUWJyMeS6"],
+    [postGameStart, "MeUWJyMeS6"],
+    [postUserNew],
+    [getUser, "n5AmNbrFeagvCS1cpffrEWAgqNw2"],
+    [postChatNew],
+    [getChat, "62feff3bf529befef32e3e28"],
+    [postChatNewMessage, "62feff3bf529befef32e3e28", "hello"],
+  ];
+
   const logApi = (apiFunction, ...args) => {
     apiFunction(...args).then((data) => {
       console.log(apiFunction.name);
+      console.log(...args);
       console.log(data);
+      setLog(apiFunction.name + "\n" + JSON.stringify(data, undefined, 2));
     });
   };
 
   return (
     <>
-      <h1>Test Page</h1>
+      <div className="d-flex">
+        <main>
+          <h1>Test Page</h1>
+          {testData.map((args) => (
+            <button
+              key={args[0].name}
+              onClick={() => logApi(...args)}
+              className="m-1"
+            >
+              {args[0].name}
+            </button>
+          ))}
+        </main>
 
-      {testData.map((args) => (
-        <button onClick={() => logApi(...args)}>{args[0].name}</button>
-      ))}
+        <textarea
+          name="log"
+          id="log"
+          cols="75"
+          rows="30"
+          value={log}
+          className="font-monospace small"
+          readOnly={true}
+        />
+      </div>
     </>
   );
 }
