da0693b71fecd5435549316239cdf5e284b62945
Yosua Nicolaus <cryodawn257@gmail.com>
2022-08-21 17:22:33 +0700

    update and clean Game

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -1,43 +1,30 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { socket } from "../../helper/socket";
+import { useSocket } from "../../helper/socket";
+// import { useAuth } from "../../helper/auth";
 import LoadingPage from "../../components/LoadingPage";
-import { useAuth } from "../../helper/auth";
 // TODO: set what user can control with auth
 
 function Game() {
   const { id } = useParams();
-  const { user } = useAuth();
-  const { uid } = user;
+  // const { uid } = useAuth();
+  const socket = useSocket(id);
   const [gameDB, setGameDB] = useState(null);
-  const [chess, setChess] = useState(null);
 
   useEffect(() => {
-    if (!id || !uid) return;
-    socket.emit("join", { id, uid });
+    console.log(`joining game ${id}`);
 
     socket.on("update-game", (game) => {
       console.log("updating gameDB state");
       setGameDB(game);
     });
 
-    socket.on("update-chess", (chess) => {
-      console.log("updating chess state");
-      setChess(chess);
-    });
-
-    socket.on("error", (message) => {
-      console.log(message);
-    });
-
     return () => {
-      console.log("leaving, turning off socket listener");
-      socket.emit("leave", { id, uid });
+      console.log(`leaving game ${id}`);
       socket.off("update-game");
-      socket.off("update-chess");
-      socket.off("log");
     };
-  }, [id, uid]);
+    // eslint-disable-next-line
+  }, []);
 
   if (!gameDB) return <LoadingPage text={"fetching game from DB..."} />;
 
@@ -47,7 +34,6 @@ function Game() {
       <div>Game ID: {id}</div>
       {gameDB && <div>{JSON.stringify(gameDB)}</div>}
       <hr />
-      {chess && <div>{JSON.stringify(chess.getMoves())}</div>}
     </main>
   );
 }
