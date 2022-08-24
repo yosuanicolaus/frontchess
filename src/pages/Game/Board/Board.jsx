d6ee11b855f2a2ab401806b15f3f3e41000d6849
Yosua Nicolaus <cryodawn257@gmail.com>
2022-08-24 13:27:39 +0700

    renders chess background, & create positions []

--- src/pages/Game/Board/Board.jsx
+++ src/pages/Game/Board/Board.jsx
@@ -1,9 +1,35 @@
+import { useGameDB } from "../GameHooks";
+
 function Board({ size }) {
+  const { game } = useGameDB();
+  const positions = createPositions(size);
+
   return (
-    <div>
-      <h1>Board</h1>
-    </div>
+    <main
+      className="position-relative"
+      style={{
+        backgroundImage: "url(/boards/blue.svg)",
+        width: size,
+        height: size,
+      }}
+    >
+      <div className="small font-monospace">hello world</div>
+    </main>
   );
+}
+
+function createPositions(size) {
+  const positions = [];
+
+  for (let rank = 0; rank < 8; rank++) {
+    positions.push([]);
+    for (let file = 0; file < 8; file++) {
+      const x = (file / 8) * size;
+      const y = (rank / 8) * size;
+      positions[rank].push({ x, y });
+    }
+  }
+  return positions;
 }
 
 export default Board;
