475f7d0de0b6ab51ba6f55efe7450cffc77b5510
Yosua Nicolaus <cryodawn257@gmail.com>
2022-08-24 08:26:07 +0700

    create Board's SizeControl (dynamic range input)

--- src/pages/Game/GameBoard.jsx
+++ src/pages/Game/GameBoard.jsx
@@ -1,18 +1,17 @@
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import { useDimensions } from "../../helper/dimensions";
 import { useGameDB } from "./GameHooks";
 import BoardEmpty from "./Board/BoardEmpty";
+import BoardWaiting from "./Board/BoardWaiting";
 import BoardPending from "./Board/BoardPending";
 import BoardReady from "./Board/BoardReady";
-import BoardWaiting from "./Board/BoardWaiting";
 import Board from "./Board/Board";
 
 function GameBoard() {
   const ref = useRef();
   const dim = useDimensions(ref);
   const { game } = useGameDB();
-
-  const size = 400;
+  const [size, setSize] = useState(450);
 
   const getBoard = (state) => {
     switch (state) {
@@ -32,14 +31,66 @@ function GameBoard() {
   };
 
   return (
-    <section ref={ref} className="board col-lg-6 col-md-7 col d-flex bg-light">
+    <section
+      ref={ref}
+      className="board col-lg-6 col-md-7 col d-flex flex-column bg-light"
+    >
       <main
         style={{ height: size, width: size }}
         className="shadow-lg m-auto d-flex text-bg-dark bg-gradient"
       >
         {getBoard(game.state)}
       </main>
+      {/* TODO: size control for mobile */}
+      <SizeControl size={size} setSize={setSize} dim={dim} />
     </section>
+  );
+}
+
+function SizeControl({ size, setSize, dim }) {
+  const min = 350;
+  const [max, setMax] = useState(600);
+
+  const setFloorSize = (value) => {
+    setSize(Math.floor(value));
+  };
+
+  const handleSlide = (e) => {
+    setFloorSize(e.target.value);
+  };
+
+  const lerp = (a, b, t) => {
+    return a + (b - a) * t;
+  };
+
+  useEffect(() => {
+    const smallerSide = Math.min(dim.width, dim.height);
+    const distance = smallerSide - min;
+    const newMax = (3 / 4) * distance + min;
+    const recommendedSize = (min + newMax) / 2;
+
+    if (newMax < min) return;
+    setMax(newMax);
+    if (size > newMax) return setSize(newMax);
+    setFloorSize(lerp(size, recommendedSize, 0.1));
+    // eslint-disable-next-line
+  }, [dim]);
+
+  return (
+    <div className="row text-bg-secondary p-2">
+      <label htmlFor="sizeControl" className="text-center small">
+        Set Board Size
+      </label>
+      <input
+        id="sizeControl"
+        type="range"
+        className="form-range"
+        min={min}
+        max={max}
+        value={size}
+        onChange={handleSlide}
+      />
+    </div>
   );
 }
 
