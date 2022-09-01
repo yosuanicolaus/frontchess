5cadc0d43ebfe5aae0c9149f8e141bc690de015b
Yosua Nicolaus <cryodawn257@gmail.com>
2022-09-02 00:11:53 +0700

    fix error in app table

--- src/App/AppTable.tsx
+++ src/App/AppTable.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Loading from "../components/Loading";
 import { useApi } from "../helper/api";
 import { GameModel } from "../helper/types";
 
@@ -6,14 +7,16 @@ function AppTable() {
   const { getGameOpen } = useApi();
   const [games, setGames] = useState<GameModel[]>([]);
 
-  const lobbyData = games.map((game, index) => {
-    return {
-      idx: index + 1,
-      name: game.user0.name,
-      elo: game.user0.elo,
-      timeControl: game.timeControl,
-    };
-  });
+  const lobbyData =
+    games &&
+    games.map((game, index) => {
+      return {
+        idx: index + 1,
+        name: game.user0.name,
+        elo: game.user0.elo,
+        timeControl: game.timeControl,
+      };
+    });
 
   useEffect(() => {
     // TODO: create socket connection and listener
@@ -22,23 +25,25 @@ function AppTable() {
     });
   }, [getGameOpen]);
 
-  return (
-    <table className="table table-hover">
-      <thead>
-        <tr className="text-bg-secondary">
-          <th scope="col">#</th>
-          <th scope="col">Username</th>
-          <th scope="col">ELO</th>
-          <th scope="col">Time Control</th>
-        </tr>
-      </thead>
+  if (!games) return <Loading text="getting open game..." />;
 
-      {lobbyData.length > 0 ? (
-        <>
-          {/* TODO: make each table row a clickable link */}
+  return (
+    <>
+      <table className="table table-hover">
+        <thead>
+          <tr className="text-bg-secondary">
+            <th scope="col">#</th>
+            <th scope="col">Username</th>
+            <th scope="col">ELO</th>
+            <th scope="col">Time Control</th>
+          </tr>
+        </thead>
+
+        {lobbyData.length > 0 && (
+          // {/* TODO: make each table row a clickable link */}
           <tbody>
             {lobbyData.map((data) => (
-              <tr>
+              <tr key={`lobby-${data.idx}`}>
                 <th scope="row">{data.idx}</th>
                 <td>{data.name}</td>
                 <td>{data.elo}</td>
@@ -46,17 +51,17 @@ function AppTable() {
               </tr>
             ))}
           </tbody>
-        </>
-      ) : (
-        <tr>
-          <td>
-            <em>
-              No open game found. Create a new game and invite your friends!
-            </em>
-          </td>
-        </tr>
+        )}
+      </table>
+
+      {lobbyData.length === 0 && (
+        <div className="p-3 text-center">
+          <em>No open game found</em>
+          <br />
+          <em>Create a new game and invite your friends!</em>
+        </div>
       )}
-    </table>
+    </>
   );
 }
 
